abd5ddcc4bc6559b96d7473eac42f0fa666ea080
WhyBlueCompany <whybluecompany@gmail.com>
2024-09-04 14:48:34 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Aquí puedes agregar funcionalidades de JavaScript
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('¡La página está completamente cargada!');
+    // Lógica para carrusel de productos y otras funcionalidades...
+});
